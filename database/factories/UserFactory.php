eacfbe9fb06d05f1109bf955fecb4c444aeddabc
Ahmed-Elimam <aa.emam97@gmail.com>
2025-06-08 03:22:13 +0300

    feat: adding factories to seed test-data

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -2,43 +2,50 @@
 
 namespace Database\Factories;
 
+use App\Models\Auth\User;
+use App\Models\Auth\Track;
 use Illuminate\Database\Eloquent\Factories\Factory;
-use Illuminate\Support\Facades\Hash;
-use Illuminate\Support\Str;
 
-/**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\User>
- */
 class UserFactory extends Factory
 {
-    /**
-     * The current password being used by the factory.
-     */
-    protected static ?string $password;
+    protected $model = User::class;
 
-    /**
-     * Define the model's default state.
-     *
-     * @return array<string, mixed>
-     */
     public function definition(): array
     {
         return [
-            'name' => fake()->name(),
-            'email' => fake()->unique()->safeEmail(),
-            'email_verified_at' => now(),
-            'password' => static::$password ??= Hash::make('password'),
-            'remember_token' => Str::random(10),
+            'portal_user_id' => 'PU' . $this->faker->unique()->bothify('#####'),
+            'email' => $this->faker->unique()->safeEmail(),
+            'first_name' => $this->faker->firstName(),
+            'last_name' => $this->faker->lastName(),
+            'phone' => $this->faker->phoneNumber(),
+            'profile_image' => $this->faker->optional()->imageUrl(200, 200, 'people'),
+            'cv_path' => $this->faker->optional()->filePath(),
+            'bio' => $this->faker->optional()->paragraph(3),
+            'linkedin_url' => $this->faker->optional()->url(),
+            'github_url' => $this->faker->optional()->url(),
+            'portfolio_url' => $this->faker->optional()->url(),
+            'track_id' => Track::inRandomOrder()->first()->id ?? Track::factory()->create()->id,
+            'intake_year' => $this->faker->optional()->numberBetween(2020, 2024),
+            'graduation_year' => $this->faker->optional()->numberBetween(2024, 2026),
+            'is_active' => $this->faker->boolean(95),
+            'last_login_at' => $this->faker->optional()->dateTimeBetween('-1 month'),
         ];
     }
 
-    /**
-     * Indicate that the model's email address should be unverified.
-     */
-    public function unverified(): static
+    public function student(): static
     {
         return $this->state(fn (array $attributes) => [
-            'email_verified_at' => null,
+            'intake_year' => $this->faker->numberBetween(2020, 2024),
+            'graduation_year' => $this->faker->numberBetween(2024, 2026),
+        ]);
+    }
+
+    public function staff(): static
+    {
+        return $this->state(fn (array $attributes) => [
+            'track_id' => null,
+            'intake_year' => null,
+            'graduation_year' => null,
         ]);
     }
 }
