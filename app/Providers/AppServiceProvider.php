2611bd9faca5aba55c804c4a5f95bfe8b4ae903b
Ahmed-Elimam <aa.emam97@gmail.com>
2025-06-29 01:04:14 +0300

    fix: fixing spatie package and applying custom middleware for OR roles

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,9 +2,13 @@
 
 namespace App\Providers;
 
+use App\Http\Middleware\CheckAnyRole;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Route;
-use App\Models\Event\Event; // Import the Event model for custom route binding
+use App\Models\Event\Event; 
+use Spatie\Permission\Middleware\PermissionMiddleware;
+use Spatie\Permission\Middleware\RoleMiddleware;
+use Spatie\Permission\Middleware\RoleOrPermissionMiddleware;// Import the Event model for custom route binding
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -27,6 +31,10 @@ class AppServiceProvider extends ServiceProvider
         //         ? Event::findOrFail($value)
         //         : Event::where('slug', $value)->firstOrFail();
         // });
+        Route::aliasMiddleware('role', RoleMiddleware::class);
+        Route::aliasMiddleware('permission', PermissionMiddleware::class);
+        Route::aliasMiddleware('role_or_permission', RoleOrPermissionMiddleware::class);
+        Route::aliasMiddleware('check.any.role', CheckAnyRole::class);
         
     }
 }
